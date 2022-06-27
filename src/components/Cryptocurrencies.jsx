6c7a2941ca4d06808d13f576240360d3805aa7a5
Abdul Rehman Aziz <fa19-bcs-016@cuilahore.edu.pk>
2022-06-27 23:55:53 +0500

    added search functionality for crypto details page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -5,16 +5,30 @@ import { Card, Row, Col, Input, Statistic, Space } from "antd";
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
 export default function Cryptocurrencies({ simplified }) {
+  const [search, setSearch] = useState("");
   const count = simplified ? 10 : 100;
   const { data, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(data?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
   console.log(cryptos);
+
+  React.useEffect(() => {
+    const filteredDate = data?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setCryptos(filteredDate);
+  }, [data, search]);
   if (isFetching) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
+      <div className="search-crypto">
+        <Input
+          placeholder="e.g. BTC"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
       {cryptos?.map((item) => {
         return (
           <Space direction="vertical" size="middle" style={{ display: "flex" }}>
