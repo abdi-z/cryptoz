65caf211815056bc2c4312d8597cd1efb3531680
Abdul Rehman Aziz <fa19-bcs-016@cuilahore.edu.pk>
2022-06-28 18:10:41 +0500

    finalized news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -5,13 +5,40 @@ import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 const { Text, Title } = Typography;
 const { options } = Select;
 
+
+const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
 export default function News({ simplified }) {
   const { data: cryptoNews } = useGetCryptoNewsQuery({
     newsCategory: "general",
-    count: simplified ? 10 : 100,
+    count: simplified ? 6 : 12,
   });
+  if (!cryptoNews?.value) return "Loading...";
 
   console.log(cryptoNews);
 
-  return <div>wow</div>;
+  return (
+    <Row gutter={[24, 24]}>
+     {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>{news.name.substring(0, 40)}...</Title>
+                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="" />
+              </div>
+              <p>{news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}</p>
+              <div className="provider-container">
+                <div>
+                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="" />
+                  <Text className="provider-name">{news.provider[0]?.name.substring(0, 12)}</Text>
+                </div>
+                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 }
